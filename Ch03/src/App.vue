339bba5c0adcfa184d8f2148434b980c3adea26c
kcy013100 <kcy013100@naver.com>
2023-04-06 12:16:00 +0900

    2023/04/06

--- Ch03/src/App.vue
+++ Ch03/src/App.vue
@@ -29,6 +29,8 @@
   <hr />
 
   <h4>4) 컴포넌트 생명주기</h4>
+  <NewComponent v-if="isShow"></NewComponent>
+  <button @click="btnStatusToggle">컴포넌트 {{ status }}</button>
   <hr />
 </template>
 
@@ -55,6 +57,7 @@ import UComponent1 from "./components/sub3/UComponent1.vue";
 import UComponent2 from "./components/sub3/UComponent2.vue";
 import UComponent3 from "./components/sub3/UComponent3.vue";
 import UComponent5 from "./components/sub3/UComponent5.vue";
+import NewComponent from "./components/sub4/NewComponent.vue";
 
 export default {
   name: "App",
@@ -65,6 +68,8 @@ export default {
       cities: ["서울", "대전", "대구", "부산", "광주"],
       value1: "",
       value2: 0,
+      status: "제거",
+      isShow: true,
     };
   },
   components: {
@@ -79,11 +84,22 @@ export default {
     UComponent2,
     UComponent3,
     UComponent5,
+    NewComponent,
+    NewComponent,
   },
   methods: {
     printValue: function (data) {
       this.value1 = data.name;
       this.value2 = data.age;
+    },
+    btnStatusToggle: function () {
+      if (this.isShow) {
+        this.isShow = false;
+        this.status = "생성";
+      } else {
+        this.isShow = true;
+        this.status = "제거";
+      }
     },
   },
 };
