7a0ba7d4edeccd5eff92f713d4c57df1568a053d
kcy013100 <kcy013100@naver.com>
2023-04-07 10:59:03 +0900

    2023/04/07

---
+++ Ch05/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <h3>Ch05. Router 실습</h3>
+
+  <h4>1) Router 기본</h4>
+  <router-link to="/hello">Hello</router-link> /
+  <router-link to="/welcome">Welcome</router-link> /
+  <router-link to="/greeting">Greeting</router-link>
+
+  <router-view></router-view>
+  <hr />
+
+  <h4>2) Nested Router</h4>
+  <hr />
+
+  <h4>3) Named View</h4>
+  <hr />
+</template>
+
+<script setup>
+/**
+ * 날짜 : 2023/04/07
+ * 이름 : 김채영
+ * 내용 :
+ *
+ * vue js 페이지 - router 들어가서 코드 카피
+ *
+ *
+ */
+</script>
+
+<style scoped></style>
