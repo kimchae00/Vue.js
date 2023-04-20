f7b5c81051cc12f82a2490b71d1048e440c8c566
kcy013100 <kcy013100@naver.com>
2023-04-20 12:46:34 +0900

    2023/04/20

---
+++ TodoApp/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <Header v-on:addTodo="addTodoHandler"></Header>
+  <Main></Main>
+  <Footer></Footer>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Main from "./components/Main.vue";
+import Footer from "./components/Footer.vue";
+import { useStore } from "vuex";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Main,
+    Footer,
+  },
+  setup() {
+    const store = useStore();
+
+    const addTodoHandler = function (value) {
+      store.dispatch("addTodo", value);
+    };
+
+    return { addTodoHandler };
+  },
+};
+</script>
+
+<style scoped></style>
